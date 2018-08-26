ed2ea9ef7dd91a63f4c93fdd6bb601b2e5d10365
saikrishnagv <saikrishnagv1996@gmail.com>
2018-08-25 22:55:44 -0400

    working. aligned osm with gt devens

--- devensData/main.py
+++ devensData/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import networkx as nx
 import pandas as pd
 from scipy.optimize import minimize
+#from scipy.optimize import Bounds
 
 
 
@@ -36,7 +37,7 @@ scan = df.iloc[0]['scan_utm']
 #print(devens_arr[100,1])
 
 def transform_devens(x):
-    devens_new = np.zeros(((len(list(devens.exterior.coords))), 2))
+    devens_new = np.zeros((devens_arr.shape[0], 2))
     for i in range(devens_arr.shape[0]):
         x_dash = x[0] * devens_arr[i,0] + x[1] * devens_arr[i,1] + x[4]
         y_dash = x[2] * devens_arr[i,0] + x[1] * devens_arr[i,1] + x[5]
@@ -47,12 +48,22 @@ def transform_devens(x):
 
     return devens_new
 
+def transform_devens_2d(x):
+    devens_new = np.zeros((devens_arr.shape[0], 2))
+    for i in range(devens_arr.shape[0]):
+        x_new = np.cos(x[0]) * devens_arr[i,0] + np.sin(x[0]) * devens_arr[i,1] + x[1]
+        y_new = -np.sin(x[0]) * devens_arr[i,0] + np.cos(x[0]) * devens_arr[i,1] + x[2]
 
+        devens_new[i,0] = x_new
+        devens_new[i,1] = y_new
+    return devens_new
 
 def find_closest(x_new, y_new):
     closest = 100000.0
     for j in range(len(xs)):
         dist = np.sqrt((x_new - xs[j])**2 + (y_new - ys[j])**2)
+        if dist == 0:
+            return dist
         if dist < closest:
             closest = dist
 
@@ -60,9 +71,10 @@ def find_closest(x_new, y_new):
 
 def cost_func(x):
     my_cost = 0
-    for i in range(devens_arr.shape[0]):
-        x_dash = x[0] * devens_arr[i,0] + x[1] * devens_arr[i,1] + x[4]
-        y_dash = x[2] * devens_arr[i,0] + x[1] * devens_arr[i,1] + x[5]
+    for i in range(devens_arr.shape[0]/10):
+        print(i)
+        x_dash = x[0] * devens_arr[i*10,0] + x[1] * devens_arr[i*10,1] + x[4]
+        y_dash = x[2] * devens_arr[i*10,0] + x[1] * devens_arr[i*10,1] + x[5]
         z_dash = 1 + x[6]
         x_new = x_dash / z_dash
         y_new = y_dash / z_dash
@@ -72,9 +84,9 @@ def cost_func(x):
 
 def cost_func_2d(x):
     my_cost = 0
-    for i in range(devens_arr.shape[0]):
-        x_new = np.cos(x[0]) * devens_arr[i,0] + np.sin(x[0]) * devens_arr[i,1] + x[1]
-        y_new = -np.sin(x[0]) * devens_arr[i,0] + np.cos(x[0]) * devens_arr[i,1] + x[2]
+    for i in range(devens_arr.shape[0]/10):
+        x_new = np.cos(x[0]) * devens_arr[i*10,0] + np.sin(x[0]) * devens_arr[i*10,1] + x[1]
+        y_new = -np.sin(x[0]) * devens_arr[i*10,0] + np.cos(x[0]) * devens_arr[i*10,1] + x[2]
         my_cost = my_cost + find_closest(x_new, y_new)
     return my_cost
 
@@ -84,13 +96,16 @@ for interior in devens.interiors:
     devens_interior += interior.coords[:]
 devens_interior = np.asarray(devens_interior)
 devens_arr = np.concatenate((devens_exterior, devens_interior), axis = 0)
+#bounds = Bounds(np.concatenate(devens_arr[:,0] - 10, devens_arr[:,1] - 10), np.concatenate(devens_arr[:,0] + 10, devens_arr[:,1] + 10))
 #print(devens_arr.shape)
 
 #print(devens.exterior.coords[1,0])
-x0 = np.array([0.5, 0.1, 0.1, 0.5, 1.0, 0.8, 1.2])
-res = minimize(cost_func, x0, method='nelder-mead', options={'xtol':1e-8, 'disp':True})
+#x0 = np.array([0.5, 0.1, 0.1, 0.5, 1.0, 0.8, 1.2])
+x0 = np.array([0.1, 0.8, 1.2])
+res = minimize(cost_func_2d, x0, method='nelder-mead', options={'xtol':1e-8, 'disp':True})
+#res_bounded = minimize(cost_func_2d, x0, method='trust-constr', options={'verbose':1}, bounds=bounds)
 print(res.x)
-devens_trans = transform_devens(res.x)
+devens_trans = transform_devens_2d(res.x)
 #print(devens_arr)
 print(devens_trans)
 #devens_arr = devens_arr.tolist()
