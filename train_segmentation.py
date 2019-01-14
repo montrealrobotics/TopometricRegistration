b7e43f70212cdd534e95b3e81ea3c54b100658fa
saikrishna-1996 <saikrishnagv1996@gmail.com>
2019-01-13 21:46:21 -0500

    bugfixes. working now

--- train_segmentation.py
+++ train_segmentation.py
@@ -37,13 +37,13 @@ print("Random Seed: ", opt.manualSeed)
 random.seed(opt.manualSeed)
 torch.manual_seed(opt.manualSeed)
 
-dataset = PartDataset(root = 'shapenetcore_partanno_segmentation_benchmark_v0', classification = False, class_choice = ['Chair'])
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
-                                          shuffle=True, num_workers=int(opt.workers))
+# dataset = PartDataset(root = 'shapenetcore_partanno_segmentation_benchmark_v0', classification = False, class_choice = ['Chair'])
+# dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
+#                                           shuffle=True, num_workers=int(opt.workers))
 
-test_dataset = PartDataset(root = 'shapenetcore_partanno_segmentation_benchmark_v0', classification = False, class_choice = ['Chair'], train = False)
-testdataloader = torch.utils.data.DataLoader(test_dataset, batch_size=opt.batchSize,
-                                          shuffle=True, num_workers=int(opt.workers))
+# test_dataset = PartDataset(root = 'shapenetcore_partanno_segmentation_benchmark_v0', classification = False, class_choice = ['Chair'], train = False)
+# testdataloader = torch.utils.data.DataLoader(test_dataset, batch_size=opt.batchSize,
+#                                           shuffle=True, num_workers=int(opt.workers))
 
 data_path = '/home/sai/data/maplite_data_chunks'
 
@@ -98,8 +98,9 @@ def normalize_input(df):
     train_output = torch.from_numpy(1*(df.iloc[0]['is_road_truth'] == True))
     return coords[0:100,:], train_output[0:100]
 
-print(len(dataset), len(test_dataset))
-num_classes = dataset.num_seg_classes
+# print(len(dataset), len(test_dataset))
+# num_classes = dataset.num_seg_classes
+num_classes = 1
 print('classes', num_classes)
 try:
     os.makedirs(opt.outf)
@@ -117,11 +118,12 @@ if opt.model != '':
 optimizer = optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9)
 classifier.cuda()
 
-num_batch = len(dataset)/opt.batchSize
+# num_batch = len(dataset)/opt.batchSize
+num_batch = 10
 
 for epoch in range(opt.nepoch):
-    # for i in range(50):
-    for i, data in enumerate(dataloader):
+    for i in range(50):
+    # for i, data in enumerate(dataloader):
         # points, target = data
         points = np.zeros((2,100,4))
         target = np.zeros((2,100))
@@ -129,43 +131,52 @@ for epoch in range(opt.nepoch):
 
                 ## Prepare data for training
         points[0,:,:], target[0,:] = normalize_input(df)
+        # for ss in range(100):
+        #     if targets[ss] == 0:
+        #         target[0,ss,0] = 1
+        #     else:
+        #         target[0,ss,1] = 1
         # points = np.expand_dims(points, axis=0)
         # target = np.expand_dims(target, axis=0)
         # df = pd.read_pickle(data_list[i])
         # points[1,:,:], target[1,:] = normalize_input(df)
         print("points.shape, target.shape = ",points.shape, target.shape)
         print(points.shape, target.shape)
-        points  = Variable(torch.FloatTensor(points)).cuda()
-        target =  Variable(torch.LongTensor(target)).cuda()
+        print(target[0,:,])
+        points  = Variable(torch.FloatTensor(points))
+        target =  Variable(torch.FloatTensor(target))
         points = points.transpose(2,1) 
         points, target = points.cuda(), target.cuda()   
         optimizer.zero_grad()
         classifier = classifier.train()
         pred, _ = classifier(points)
         pred = pred.view(-1, num_classes)
+        # pred = pred.squeeze()
         target = target.view(-1,1)[:,0] - 1
-        #print(pred.size(), target.size())
-        loss = F.nll_loss(pred, target)
+        target = target.unsqueeze(1)
+        print(pred.size(), target.size())
+
+        loss = F.mse_loss(pred, target)
         loss.backward()
         optimizer.step()
         pred_choice = pred.data.max(1)[1]
-        correct = pred_choice.eq(target.data).cpu().sum()
+        correct = pred_choice.eq(target.long().data).cpu().sum()
         print('[%d: %d/%d] train loss: %f accuracy: %f' %(epoch, i, num_batch, loss.item(), correct.item()/float(opt.batchSize * 2500)))
         
-        if i % 10 == 0:
-            j, data = next(enumerate(testdataloader, 0))
-            points, target = data
-            points, target = Variable(points), Variable(target)
-            points = points.transpose(2,1) 
-            points, target = points.cuda(), target.cuda()
-            classifier = classifier.eval()
-            pred, _ = classifier(points)
-            pred = pred.view(-1, num_classes)
-            target = target.view(-1,1)[:,0] - 1
+        # if i % 10 == 0:
+        #     j, data = next(enumerate(testdataloader, 0))
+        #     points, target = data
+        #     points, target = Variable(points), Variable(target)
+        #     points = points.transpose(2,1) 
+        #     points, target = points.cuda(), target.cuda()
+        #     classifier = classifier.eval()
+        #     pred, _ = classifier(points)
+        #     pred = pred.view(-1, num_classes)
+        #     target = target.view(-1,1)[:,0] - 1
 
-            loss = F.nll_loss(pred, target)
-            pred_choice = pred.data.max(1)[1]
-            correct = pred_choice.eq(target.data).cpu().sum()
-            print('[%d: %d/%d] %s loss: %f accuracy: %f' %(epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize * 2500)))
+        #     loss = F.nll_loss(pred, target)
+        #     pred_choice = pred.data.max(1)[1]
+        #     correct = pred_choice.eq(target.data).cpu().sum()
+        #     print('[%d: %d/%d] %s loss: %f accuracy: %f' %(epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize * 2500)))
     
     torch.save(classifier.state_dict(), '%s/seg_model_%d.pth' % (opt.outf, epoch))
