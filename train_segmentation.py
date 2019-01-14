fec4b7df5a393e14249a5c5e8c609dc845e9e64a
saikrishna-1996 <saikrishnagv1996@gmail.com>
2019-01-13 22:03:26 -0500

    clean up

--- train_segmentation.py
+++ train_segmentation.py
@@ -37,39 +37,17 @@ print("Random Seed: ", opt.manualSeed)
 random.seed(opt.manualSeed)
 torch.manual_seed(opt.manualSeed)
 
-# dataset = PartDataset(root = 'shapenetcore_partanno_segmentation_benchmark_v0', classification = False, class_choice = ['Chair'])
-# dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
-#                                           shuffle=True, num_workers=int(opt.workers))
-
-# test_dataset = PartDataset(root = 'shapenetcore_partanno_segmentation_benchmark_v0', classification = False, class_choice = ['Chair'], train = False)
-# testdataloader = torch.utils.data.DataLoader(test_dataset, batch_size=opt.batchSize,
-#                                           shuffle=True, num_workers=int(opt.workers))
-
 data_path = '/home/sai/data/maplite_data_chunks'
 
 ### getting list of the data
 data_list = glob.glob(data_path+'/*.pkl')
 data_list.sort()
-
-### Number of inputs 
 data_length = len(data_list)
 
 ### Getting train and test data in random order
 indices = np.random.permutation(data_length)
-
-### Dividing training and test set in some ratio
-### For 1000 points, 250 points for training and 750 points for testing, then ratio is 1:3
-train_part = 1
-test_part = 2
-
-### Defining criterion for the neural network
-# criterion = criterion
-
-### getting train and test indices(randomly)
-train_indices = indices[0:int(train_part*data_length/(train_part + test_part))]
-test_indices = indices[int(train_part*data_length/(train_part + test_part)):-1]
-
-df = pd.read_pickle(data_list[0])
+train_indices = indices[0:int(2*data_length/(3))]
+test_indices = indices[int(data_length/(3)):-1]
 
 def normalize_input(df):
 
@@ -86,9 +64,6 @@ def normalize_input(df):
     y = 150*y/max(y) 
     z = 150*z/max(z) 
 
-    ### Normalizing features?? Not decided yet
-
-    ### final train and test data
     coords = torch.randn(len(x), 4)
     coords[:,0] = torch.from_numpy(x.copy())
     coords[:,1] = torch.from_numpy(y.copy())
@@ -97,9 +72,8 @@ def normalize_input(df):
     del x, y, z
     train_output = torch.from_numpy(1*(df.iloc[0]['is_road_truth'] == True))
     return coords[0:100,:], train_output[0:100]
+    # return coords, train_output
 
-# print(len(dataset), len(test_dataset))
-# num_classes = dataset.num_seg_classes
 num_classes = 1
 print('classes', num_classes)
 try:
@@ -123,23 +97,11 @@ num_batch = 10
 
 for epoch in range(opt.nepoch):
     for i in range(50):
-    # for i, data in enumerate(dataloader):
-        # points, target = data
-        points = np.zeros((2,100,4))
-        target = np.zeros((2,100))
+        points = np.zeros((2,111168,4))
+        target = np.zeros((2,111168))
         df = pd.read_pickle(data_list[i])
 
-                ## Prepare data for training
         points[0,:,:], target[0,:] = normalize_input(df)
-        # for ss in range(100):
-        #     if targets[ss] == 0:
-        #         target[0,ss,0] = 1
-        #     else:
-        #         target[0,ss,1] = 1
-        # points = np.expand_dims(points, axis=0)
-        # target = np.expand_dims(target, axis=0)
-        # df = pd.read_pickle(data_list[i])
-        # points[1,:,:], target[1,:] = normalize_input(df)
         print("points.shape, target.shape = ",points.shape, target.shape)
         print(points.shape, target.shape)
         print(target[0,:,])
